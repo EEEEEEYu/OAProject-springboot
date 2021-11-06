1713383467435bd294a5584e8ece86ca9b4ecabf
EEEEEEYu <elvisyuovo@gmail.com>
2021-11-05 21:08:19 -0400

    finish

--- src/main/java/com/example/chainanalysis/Controller/ExchangeController.java
+++ src/main/java/com/example/chainanalysis/Controller/ExchangeController.java
@@ -4,11 +4,13 @@ import com.example.chainanalysis.Model.Exchange;
 import com.example.chainanalysis.Service.ExchangeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ExchangeController {
     private final ExchangeService exchangeService;
 
--- src/main/java/com/example/chainanalysis/Controller/PriceController.java
+++ src/main/java/com/example/chainanalysis/Controller/PriceController.java
@@ -7,6 +7,7 @@ import com.example.chainanalysis.Service.ExchangeService;
 import com.example.chainanalysis.Service.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Time;
@@ -14,6 +15,7 @@ import java.time.LocalTime;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 public class PriceController {
     private final PriceService priceService;
     private final ExchangeService exchangeService;
